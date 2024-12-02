e0a416625c6f098a9aa8e38eff3c7b3e5af8d94e
fabien-chevalley <fabien.chevalley@gmail.com>
2024-12-02 11:00:27 +0100

    day2

---
+++ aoc-2024/Puzzles/Day2Puzzle.cs
@@ -0,0 +1,83 @@
+using Levels = System.Collections.Generic.List<long>;
+
+namespace aoc_2024.Puzzles;
+
+public class Day2Puzzle : Puzzle<long>
+{
+    public override async ValueTask<long> PartOne()
+    {
+        var lines = await GetLines();
+
+        var sum = 0;
+        foreach (var levels in lines)
+            if (IsSafe(levels))
+                sum++;
+
+        // 218
+        return sum;
+    }
+
+    public override async ValueTask<long> PartTwo()
+    {
+        var lines = await GetLines();
+
+        var sum = 0;
+        foreach (var levels in lines)
+            if (IsSafe(levels))
+                sum++;
+            else
+                for (var i = 0; i < levels.Count; i++)
+                {
+                    var levelsCopy = levels.ToList();
+                    levelsCopy.RemoveAt(i);
+                    if (IsSafe(levelsCopy))
+                    {
+                        sum++;
+                        break;
+                    }
+                }
+
+        // 290
+        return sum;
+    }
+
+    private static bool IsSafe(Levels levels)
+    {
+        if (levels.Distinct().Count() == 1)
+        {
+            return false;
+        }
+
+        var fail = false;
+        long? previousDiff = null;
+        for (var i = 0; i < levels.Count - 1; i++)
+        {
+            var diff = levels[i] - levels[i + 1];
+            if (Math.Abs(diff) > 3)
+            {
+                fail = true;
+                break;
+            }
+
+            if (previousDiff != null && Math.Sign(previousDiff.Value) != Math.Sign(diff))
+            {
+                fail = true;
+                break;
+            }
+
+            previousDiff = diff;
+        }
+
+        return !fail;
+    }
+
+    private async Task<List<Levels>> GetLines()
+    {
+        var lines = new List<Levels>();
+        
+        foreach (var line in await File.ReadAllLinesAsync(Filename))
+            lines.Add(line.Split(' ').Select(long.Parse).ToList());
+
+        return lines;
+    }
+}
