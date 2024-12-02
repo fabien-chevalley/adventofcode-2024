f06cc358cdea6aaa5957896ba988c7e798f295fb
fabien-chevalley <fabien.chevalley@gmail.com>
2024-12-02 11:01:48 +0100

    chore: improve loading

--- aoc-2024/Solver.cs
+++ aoc-2024/Solver.cs
@@ -3,35 +3,71 @@ using Spectre.Console;
 
 namespace aoc_2024;
 
-public static class Solver
+public class Solver
 {
-    public static async Task Solve<TPuzzle, TResult>() where TPuzzle : Puzzle<TResult>, new()
-    {
-        var puzzle = new TPuzzle();
+    private readonly Table _table;
 
-        var table = new Table()
+    public Solver()
+    {
+        _table = new Table()
             .AddColumns(
                 "[bold]Puzzle[/]",
-                "[bold]Part[/]",
-                "[bold]Solution[/]",
+                "[bold]Part one[/]",
+                "[bold]Part two[/]",
                 "[bold]Elapsed time[/]")
             .RoundedBorder()
             .BorderColor(Color.Grey);
+    }
 
-        await AnsiConsole.Live(table)
+    public async Task SolveLast()
+    {
+        var type = typeof(Puzzle<>).Assembly
+            .GetTypes()
+            .Where(x => !x.IsAbstract && x.IsAssignableTo(typeof(IPuzzle)))
+            .OrderBy(x => x.Name)
+            .Last();
+
+        await Solve(type);
+    }
+
+    public async Task SolveAll()
+    {
+        var types = typeof(Puzzle<>).Assembly
+            .GetTypes()
+            .Where(x => !x.IsAbstract && x.IsAssignableTo(typeof(IPuzzle)))
+            .OrderBy(x => x.Name);
+
+        foreach (var type in types) await Solve(type);
+    }
+
+    public async Task Solve(Type type)
+    {
+        var puzzle = Activator.CreateInstance(type) as Puzzle<long>;
+
+        await AnsiConsole.Live(_table)
             .StartAsync(async ctx =>
             {
                 var stopwatch = Stopwatch.StartNew();
                 var partOneResult = await puzzle.PartOne();
-                table.AddRow(puzzle.Name, "one", partOneResult.ToString(), stopwatch.ElapsedMilliseconds.ToString());
-
+                var partOneDuration = stopwatch.Elapsed;
+                _table.AddRow(puzzle.Name, partOneResult.ToString(), string.Empty, $"{partOneDuration.Milliseconds}ms");
                 ctx.Refresh();
 
                 stopwatch.Restart();
                 var partTwoResult = await puzzle.PartTwo();
-                table.AddRow(puzzle.Name, "two", partTwoResult.ToString(), stopwatch.ElapsedMilliseconds.ToString());
-
+                var partTwoDuration = stopwatch.Elapsed;
+                _table.UpdateCell(_table.Rows.Count - 1, 2, partTwoResult.ToString());
+                _table.UpdateCell(_table.Rows.Count - 1, 3,
+                    $"{partOneDuration.Milliseconds}ms / {partTwoDuration.Milliseconds}ms");
                 ctx.Refresh();
             });
+    }
+
+
+    public async Task Solve<TPuzzle, TResult>()
+        where TPuzzle : Puzzle<TResult>, new()
+        where TResult : new()
+    {
+        await Solve(typeof(TPuzzle));
     }
 }
