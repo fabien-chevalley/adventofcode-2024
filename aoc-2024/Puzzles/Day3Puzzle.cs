5e8764ff64411b403b14cd4543c5a4c519be23b9
fabien-chevalley <fabien.chevalley@elca.ch>
2024-12-03 09:15:10 +0100

    day 3 part 2

--- aoc-2024/Puzzles/Day3Puzzle.cs
+++ aoc-2024/Puzzles/Day3Puzzle.cs
@@ -19,6 +19,25 @@ public class Day3Puzzle : Puzzle<long>
 
     public override async ValueTask<long> PartTwo()
     {
-        return 0;
+        var line = await File.ReadAllTextAsync(Filename);
+
+        var sum = 0;
+        var matches = Regex.Matches(line, @"(mul\((\d{1,3}),(\d{1,3})\))");
+        var doIndexes = Regex.Matches(line, @"do\(\)").Select(m => m.Index).ToArray();
+        var dontIndexes = Regex.Matches(line, @"don't\(\)").Select(m => m.Index).ToArray();
+        
+        foreach (Match match in matches)
+        {
+            var index = match.Index;
+
+            var previousDo = doIndexes.LastOrDefault(x => x < index);
+            var previousDont = dontIndexes.LastOrDefault(x => x < index);
+
+            if (previousDont == 0 || previousDo > previousDont)
+            {
+                sum += int.Parse(match.Groups[2].Value) * int.Parse(match.Groups[3].Value);
+            }
+        }
+        return sum;
     }
 }
